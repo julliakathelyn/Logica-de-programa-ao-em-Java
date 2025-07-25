6ba5e4bed37ddcca9a0f15086928f9245926a0d2
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2025-07-24 21:09:28 -0300

    exercicio 2

---
+++ exercicio2Java/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String nome;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite seu nome");
+        nome = teclado.next();
+
+
+        System.out.println("Olá " + nome );
+
+
+
+
+
+
+
+
+    }
+}
